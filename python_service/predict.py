1018b6976a88d0a7cbdc8abaf00f461de5cd590c
killerzdouou <charles.menard@live.fr>
2018-02-03 17:51:28 -0500

    lol

--- python_service/predict.py
+++ python_service/predict.py
@@ -1,14 +1,22 @@
 from saver import Saver
 from preprocess_img import Preprocessor
 
-def predict(model_name="number",image):
+
+def predict(model_name="number", img):
+
+    pre = Preprocessor()
     #loading the pickle
     sav=Saver()
-    sav.load(model_name+"model")
+    model = sav.load("trained_models/", model_name+ "_model")
 
     #preprocessing of images
-    pre = Preprocessor()
-    
+    im = pre.processImage("img/" + "testdeux.jpg", 8, 8)
+
+    return model.predict(im)
 
 
-predict(image)
+
+
+
+
+print(predict())
